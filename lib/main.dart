d17a6c5dae6da2eb29bf72de15f281c29ddb63f6
Mansour <m2.mansour250@gmail.com>
2023-05-22 11:22:15 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:chat_gpt_02/test/test_one.dart';
+import 'package:flutter/material.dart';
+
+import 'chat_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ChatGPT Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        useMaterial3: true,
+      ),
+      home:  ChatPage(),
+    );
+  }
+}
